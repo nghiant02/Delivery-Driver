015887b6fc0f510c2b702ee2ddeb6c5251b93687
Leo <nghia2852002@gmail.com>
2024-01-27 15:16:12 +0700

    first commit

---
+++ Assets/Script/Delivery.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+    bool hasPackage = false;
+    [SerializeField] float destroyDelay; 
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Boom!!!");
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Package" && hasPackage == false)
+        {
+            hasPackage = true;
+            Debug.Log("Package pick up");
+
+            Destroy(collision.gameObject, destroyDelay);
+        }
+
+        if (collision.tag == "Customer" && hasPackage == true)
+        {
+            hasPackage = false;
+            Debug.Log("Package is delivered");
+        }
+    }
+}
---
+++ Assets/Script/Driver.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 3.8f;
+    [SerializeField] float rotateSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotateInput = Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime;
+        float moveInput = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        transform.Rotate(0, 0, -rotateInput);
+
+        transform.Translate(0, moveInput, 0);
+    }
+}
