f39bd97de783184a5400945973a8feb90aae2152
thangnguyen8520 <130530198+thangnguyen8520@users.noreply.github.com>
2024-03-18 01:20:42 +0700

    Revert "Revert "Add prefab for spawn random package""
    This reverts commit e3858fbe2e852ab69fc012b5b8c67ef2706d7ac0.

--- Assets/Script/ObjectSpawner.cs
+++ Assets/Script/ObjectSpawner.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectSpawner : MonoBehaviour
@@ -7,15 +8,25 @@ public class ObjectSpawner : MonoBehaviour
     public GameObject slowDownPrefab;
     public string roadTag = "Road"; // The tag used for your road objects
     public int numberOfEachType = 4; // Number of each object type to spawn
+    List<GameObject> package;
+    List<GameObject> speedUp;
+    List<GameObject> slowDown;
 
     void Start()
     {
+        package = new List<GameObject>();
+        speedUp = new List<GameObject>();
+        slowDown = new List<GameObject>();
+        Debug.Log("Package Prefab Assigned: " + (packagePrefab != null));
+        Debug.Log("Speed Up Prefab Assigned: " + (speedUpPrefab != null));
+        Debug.Log("Slow Down Prefab Assigned: " + (slowDownPrefab != null));
         SpawnObjectsOnRoad();
     }
 
     void SpawnObjectsOnRoad()
     {
         GameObject[] roads = GameObject.FindGameObjectsWithTag(roadTag);
+        Debug.Log("Roads found: " + roads.Length);
 
         // Spawn Packages
         SpawnObjects(packagePrefab, roads, numberOfEachType);
@@ -32,19 +43,27 @@ public class ObjectSpawner : MonoBehaviour
         for (int i = 0; i < quantity; i++)
         {
             GameObject road = roads[Random.Range(0, roads.Length)]; // Pick a random road
-            Collider2D roadCollider = road.GetComponent<Collider2D>();
-            if (roadCollider != null)
-            {
-                Vector2 spawnPosition = RandomPositionInBounds(roadCollider.bounds);
-                Instantiate(prefab, spawnPosition, Quaternion.identity);
-            }
+            Vector2 spawnPosition = RandomPositionInBounds(road);
+            GameObject newObj = Instantiate(prefab, spawnPosition, Quaternion.identity); // Create the new object
+            Debug.Log("Spawned Object: " + newObj.name);
+
+            // Assign a name based on the prefab type and index
+            List<GameObject> targetList = prefab == packagePrefab ? package : prefab == speedUpPrefab ? speedUp : slowDown;
+            newObj.name = prefab.name + " #" + (targetList.Count + 1);
+            targetList.Add(newObj); // Add the new object to the list
         }
     }
 
-    Vector2 RandomPositionInBounds(Bounds bounds)
+    Vector2 RandomPositionInBounds(GameObject road)
     {
-        float x = Random.Range(bounds.min.x, bounds.max.x);
-        float y = Random.Range(bounds.min.y, bounds.max.y);
-        return new Vector2(x, y);
+        Collider2D roadCollider = road.GetComponent<Collider2D>();
+        if (roadCollider != null)
+        {
+            Bounds bounds = roadCollider.bounds;
+            float x = Random.Range(bounds.min.x, bounds.max.x);
+            float y = Random.Range(bounds.min.y, bounds.max.y);
+            return new Vector2(x, y);
+        }
+        return Vector2.zero; // Return a default value if no collider is found
     }
 }
