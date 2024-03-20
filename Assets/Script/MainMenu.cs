c125930c9bcb589d9d6f53e56ed3ac4c5e1200cb
thangnguyen8520 <130530198+thangnguyen8520@users.noreply.github.com>
2024-03-20 14:41:02 +0700

    abc

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -33,31 +33,31 @@ public class MainMenu : Menu
             loadGameButton.interactable = false;
         }
     }
-    private IEnumerator AutoSave()
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(5f);
-            foreach (IDataPersistence dataPersistenceObj in dataPersistenceObjects)
-            {
-                dataPersistenceObj.SaveData(gameData);
-            }
+    //private IEnumerator AutoSave()
+    //{
+    //    while (true)
+    //    {
+    //        yield return new WaitForSeconds(5f);
+    //        foreach (IDataPersistence dataPersistenceObj in dataPersistenceObjects)
+    //        {
+    //            dataPersistenceObj.SaveData(gameData);
+    //        }
 
-            Debug.Log($"GameData is null: {gameData == null}");
+    //        Debug.Log($"GameData is null: {gameData == null}");
 
-            // timestamp the data so we know when it was last saved
-            gameData.lastUpdated = System.DateTime.Now.ToBinary();
+    //        // timestamp the data so we know when it was last saved
+    //        gameData.lastUpdated = System.DateTime.Now.ToBinary();
 
-            // save that data to a file using the data handler
-            dataHandler.Save(gameData, selectedProfileId);
-            Debug.Log("Auto Saved Game");
-        }
-    }
+    //        // save that data to a file using the data handler
+    //        dataHandler.Save(gameData, selectedProfileId);
+    //        Debug.Log("Auto Saved Game");
+    //    }
+    //}
     public void OnNewGameClicked()
     {
         saveSlotsMenu.ActivateMenu(false);
         this.DeactivateMenu();
-        AutoSave();
+        //AutoSave();
         //SceneManager.LoadScene("Game");
         //this.gameData = new GameData();
         //DataPersistenceManager.instance.ChangeSelectedProfileId(saveSlot.GetProfileId());
